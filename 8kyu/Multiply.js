b107fce96e1e10f937e5bd6ec3495377c620b30d
Anna Kazak <anna.s.kazak@gmail.com>
2024-09-05 17:33:57 +0200

    Multiply.js

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,11 @@
+// Description:
+//     This code does not execute properly. Try to figure out why.
+//     function multiply(a, b){
+//     a * b
+// }
+
+// SOLUTION
+
+function multiply(a, b){
+    return  a * b
+}
