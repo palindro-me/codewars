22130d77e9784fa601f4cabeb9f0219da68f316a
Anna Kazak <anna.s.kazak@gmail.com>
2025-07-03 21:45:20 +0200

    String to Array

---
+++ 8kyu/Convert a string to an array.tsx
@@ -0,0 +1,11 @@
+// ⁉️Write a function to split a string and convert it into an array of words.
+//
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+// ✅Solution:
+function stringToArray(string){
+
+	// code code code
+  return string.split(' ')
+
+}
